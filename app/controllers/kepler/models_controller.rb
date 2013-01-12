86f719fe9787c75b9324378b2bfecc6c2b18f749
James Dabbs <jdabbs@emcien.com>
2013-01-12 18:23:37 -0500

    Use railroady gem for the heavy lifting.

--- app/controllers/kepler/models_controller.rb
+++ app/controllers/kepler/models_controller.rb
@@ -1,9 +1,14 @@
 module Kepler
   class ModelsController < Kepler::ApplicationController
     def index
-      Dir.chdir(Rails.root.join('app/models')) do
-        @models = Dir['**/*.rb'].map {|f| get_model(f)}.compact
-      end
+      require 'railroady'
+
+      @data = ModelsDiagram.new
+      @data.generate
+      
+      @graph = @data.instance_eval  { @graph }
+      @nodes = @graph.instance_eval { @nodes }
+      @edges = @graph.instance_eval { @edges }
     end
 
     def show
