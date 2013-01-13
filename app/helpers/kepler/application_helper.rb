6b3c5d67c4c97efa2478546a4f24bbee5d7e79cf
James Dabbs <jdabbs@emcien.com>
2013-01-12 23:51:31 -0500

    Add associations to Model#show.

--- app/helpers/kepler/application_helper.rb
+++ app/helpers/kepler/application_helper.rb
@@ -1,5 +1,9 @@
 module Kepler
   module ApplicationHelper
+    def class_link(klass)
+      link_to(klass.name, model_path(klass.name))
+    end
+    
     def format_source(path)
       CodeRay.scan(File.read(path), :ruby).div(:line_numbers => :table)
     end
